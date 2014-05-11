6e4c9507aae2b65bdb14c82fd372e0822d8af825
colca <colca7@gmail.com>
2014-05-10 18:06:32 -0700

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140507004731) do
+ActiveRecord::Schema.define(:version => 20140509174351) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
 
   create_table "popsicles", :primary_key => "pop_id", :force => true do |t|
     t.string  "flavor", :limit => 50, :null => false
